163c0eec410b0eb2006f7274baff406c9150c24d
DangerMaster451 <andrewrpelton@outlook.com>
2023-07-07 10:58:41 -0700

    updated game window

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import pygame
 from sys import exit
 
+# Basic Setup
 pygame.init()
-resx = 1280
-resy = 720
-screen = pygame.display.set_mode((resx,resy))
+DEFAULT_RESOLUTION = (1280, 720)
+screen = pygame.display.set_mode(DEFAULT_RESOLUTION, pygame.RESIZABLE | pygame.SCALED)
+pygame.display.set_caption("Shopkeeper")
 clock = pygame.time.Clock()
 
+# Game Loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
     screen.fill((94,129,162))
-    pygame.display.update()
+    pygame.display.flip()
     clock.tick(60)
