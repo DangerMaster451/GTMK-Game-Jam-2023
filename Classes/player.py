8cd08f1ea361b1c215897dfae3e4c4d6c4d1c6bc
DangerMaster451 <andrewrpelton@outlook.com>
2023-07-07 16:02:18 -0700

    player moves at uniform speed

--- Classes/player.py
+++ Classes/player.py
@@ -15,7 +15,7 @@ class Player(pygame.sprite.Sprite):
         self.move()
         self.render()
 
-    def move(self) -> None:
+    def move_old(self) -> None:
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w] or keys[pygame.K_UP]:
             self.position.y -= self.speed
@@ -28,6 +28,38 @@ class Player(pygame.sprite.Sprite):
             self.wobble_value += 1
         if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             self.position.x += self.speed
+            self.wobble_value += 1
+
+    def move(self) -> None:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w] and not keys[pygame.K_a] and not keys[pygame.K_d]: # up
+            self.position.y -= self.speed
+            self.wobble_value += 1
+        elif keys[pygame.K_s] and not keys[pygame.K_a] and not keys[pygame.K_d]: # down
+            self.position.y += self.speed
+            self.wobble_value += 1
+        elif keys[pygame.K_a] and not keys[pygame.K_w] and not keys[pygame.K_s]: # left
+            self.position.x -= self.speed
+            self.wobble_value += 1
+        elif keys[pygame.K_d] and not keys[pygame.K_w] and not keys[pygame.K_s]: # right
+            self.position.x += self.speed
+            self.wobble_value += 1
+        
+        elif keys[pygame.K_w] and keys[pygame.K_a] and not keys[pygame.K_d]:  # up left
+            self.position.x -= self.speed/2
+            self.position.y -= self.speed/2
+            self.wobble_value += 1
+        elif keys[pygame.K_w] and keys[pygame.K_d] and not keys[pygame.K_a]:  # up right
+            self.position.x += self.speed/2
+            self.position.y -= self.speed/2
+            self.wobble_value += 1
+        elif keys[pygame.K_s] and keys[pygame.K_a] and not keys[pygame.K_d]: # down left
+            self.position.x -= self.speed/2
+            self.position.y += self.speed/2
+            self.wobble_value += 1
+        elif keys[pygame.K_s] and keys[pygame.K_d] and not keys[pygame.K_a]: # down right
+            self.position.x += self.speed/2
+            self.position.y += self.speed/2
             self.wobble_value += 1
 
     def render(self) -> None:
