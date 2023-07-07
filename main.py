254b4897127a86a0e5294ec0d06c6c8579659bda
DangerMaster451 <andrewrpelton@outlook.com>
2023-07-07 11:09:02 -0700

    Added player rendering and movement

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pygame
 from sys import exit
 
+from player import Player
+
 # Basic Setup
 pygame.init()
 DEFAULT_RESOLUTION = (1280, 720)
 screen = pygame.display.set_mode(DEFAULT_RESOLUTION, pygame.RESIZABLE | pygame.SCALED)
 pygame.display.set_caption("Shopkeeper")
 clock = pygame.time.Clock()
+
+# Create Objects
+player = Player(screen)
 
 # Game Loop
 while True:
@@ -16,5 +21,9 @@ while True:
             exit()
 
     screen.fill((94,129,162))
+
+    # Render Sprites
+    player.update()
+
     pygame.display.flip()
     clock.tick(60)
---
+++ player.py
@@ -0,0 +1,29 @@
+import pygame
+from pygame.math import Vector2
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, window:pygame.Surface) -> None:
+        pygame.sprite.Sprite.__init__(self)
+        self.window = window
+        self.position = Vector2(self.window.get_size()[0]/2, self.window.get_size()[1]/2)
+        self.image = pygame.image.load("Assets/Tiles/Colored/tile_0000.png")
+        self.speed = 10
+
+
+    def update(self) -> None:
+        self.move()
+        self.render()
+
+    def move(self) -> None:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            self.position.y -= self.speed
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            self.position.y += self.speed
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            self.position.x -= self.speed
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            self.position.x += self.speed
+
+    def render(self) -> None:
+        self.window.blit(self.image, self.position)
