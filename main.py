68e09101ac790ee43a5e2d1d50e9355a1bd39b9f
XxtraBlood <tristankrizanpoop@gmail.com>
2023-07-07 10:21:00 -0700

    basics

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import pygame
+from sys import exit
+
+pygame.init()
+resx = 1280
+resy = 720
+screen = pygame.display.set_mode((resx,resy))
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    screen.fill((94,129,162))
+    pygame.display.update()
+    clock.tick(60)
